e95ca9aff03f2458090f70e8603e99319877671b
Lautaro <pelullolautaro@gmail.com>
2025-10-28 18:02:42 -0300

    Tp cSharp

---
+++ Figura.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Drawing;
+
+namespace Figuras
+{
+    public class Figura
+    {
+        public virtual void Dibujar(Pen pen, Graphics graphics, int x, int y)
+        { 
+            
+        }
+
+        public virtual void Escalar(float crecimiento) { }
+    }
+
+
+    public class Rectangulo : Figura
+    {
+        protected int alto;
+        protected int ancho;
+        
+        // Constructor
+        public Rectangulo(int ancho, int alto) 
+        {
+            this.ancho = ancho;
+            this.alto = alto;
+        }
+
+        public override void Dibujar(Pen pen, Graphics graphics, int x, int y)
+        {
+            Point[] points = new Point[4]
+            { 
+                new Point(x,y), 
+                new Point(x+ancho,y), 
+                new Point(x+ancho,y+alto), 
+                new Point(x,y+alto) 
+            };
+            // DrawPolygon dibuja un poligono dado un conjunto de puntos y un lapiz
+            graphics.DrawPolygon(pen, points);
+        }
+
+        public override void Escalar(float crecimiento) {
+            ancho = (int)(ancho * crecimiento);
+            alto = (int)(alto * crecimiento);
+        }
+    }
+
+    public class Cuadrado : Rectangulo
+    {
+        // Constructor. Un cuadrado es un rectangulo con ancho = alto
+        public Cuadrado(int lado) : base(lado,lado)
+        {
+        }
+
+        public override void Escalar(float crecimiento) {
+            ancho = (int)(ancho * crecimiento);
+            alto = (int)(alto * crecimiento);
+        }
+
+    }
+
+
+    public class Circulo : Figura
+    {
+        private int radio;
+
+        // Constructor
+        public Circulo(int radio)
+        {
+            this.radio= radio;
+        }
+
+        public override void Dibujar(Pen pen, Graphics graphics, int x, int y)
+        {
+            graphics.DrawEllipse(pen,x,y, radio, radio);
+        }
+
+        public override void Escalar(float crecimiento) {
+            radio = (int)(radio * crecimiento);
+        }
+    }
+
+    public class TrianguloIsosceles : Figura {
+    
+        private int baseTriangulo;
+        private int altura;
+
+        public TrianguloIsosceles(int baseTriangulo, int altura) {
+            this.baseTriangulo = baseTriangulo;
+            this.altura = altura;
+        }
+
+        public override void Dibujar(Pen pen, Graphics graphics, int x, int y)  {
+            Point[] puntos =
+            {
+                new Point(x, y + altura),
+                new Point(x + baseTriangulo / 2, y),
+                new Point(x + baseTriangulo, y + altura)
+            };
+            graphics.DrawPolygon(pen, puntos);
+        }
+    }
+
+    public class TrianguloEquilatero : Figura {
+        private int lado;
+
+        public TrianguloEquilatero(int lado)
+        {
+            this.lado = lado;
+        }
+
+        public override void Dibujar(Pen pen, Graphics graphics, int x, int y)
+        {
+            double altura = Math.Sqrt(3) / 2 * lado;
+            Point[] puntos =
+            {
+                new Point(x, (int)(y + altura)),
+                new Point(x + lado / 2, y),
+                new Point(x + lado, (int)(y + altura))
+            };
+            graphics.DrawPolygon(pen, puntos);
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Figuras
+{
+    public partial class Form1 : Form
+    {
+        Figura[] figuras;
+
+        public Form1()
+        {
+            InitializeComponent();
+            figuras = new Figura[5]     
+            {
+                new Circulo(60),
+                new Rectangulo(30,50),
+                new Cuadrado(45),
+                new TrianguloIsosceles(60, 80),
+                new TrianguloEquilatero(60)
+            };
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics gr = pictureBox1.CreateGraphics();
+            
+            Random rand = new Random();
+            
+            Color[] colores = new Color[figuras.Length];
+            
+            for (int i = 0; i < figuras.Length; i++) {
+                colores[i] = Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256));
+            }
+
+            for (int i = 0; i < figuras.Length; i++) {
+
+                float crecimiento = 1 + 0.5f * i;
+                figuras[i].Escalar(crecimiento);
+                using (Pen pen = new Pen(colores[i])) {
+                    figuras[i].Dibujar(pen, gr, i * 120, 50);
+                }
+            }
+
+        }
+    }
+}
